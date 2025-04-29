071a34b5d58a615467f3cbcdc6379a03ec44c8cc
niuqy <2318607163@qq.com>
2025-04-29 19:20:19 +0800

    使用更新的stable diffusion模型

--- visual_chatgpt.py
+++ visual_chatgpt.py
@@ -13,7 +13,7 @@ from transformers import AutoModelForCausalLM, AutoTokenizer, CLIPSegProcessor, 
 from transformers import pipeline, BlipProcessor, BlipForConditionalGeneration, BlipForQuestionAnswering
 from transformers import AutoImageProcessor, UperNetForSemanticSegmentation
 
-from diffusers import StableDiffusionPipeline, StableDiffusionInpaintPipeline, StableDiffusionInstructPix2PixPipeline
+from diffusers import StableDiffusionPipeline, StableDiffusionXLPipeline, StableDiffusionInpaintPipeline, StableDiffusionInstructPix2PixPipeline
 from diffusers import EulerAncestralDiscreteScheduler
 from diffusers import StableDiffusionControlNetPipeline, ControlNetModel, UniPCMultistepScheduler
 from controlnet_aux import OpenposeDetector, MLSDdetector, HEDdetector
@@ -321,6 +321,40 @@ class Text2Image:
         print(
             f"\nProcessed Text2Image, Input Text: {text}, Output Image: {image_filename}")
         return image_filename
+
+
+class Text2ImagePro:
+    def __init__(self, device):
+        print("Initializing Text2Image to %s" % device)
+        self.device = device
+        self.torch_dtype = torch.float16 if 'cuda' in device else torch.float32
+        self.pipe = StableDiffusionXLPipeline.from_pretrained(
+            "stabilityai/stable-diffusion-xl-base-1.0",
+            torch_dtype=self.torch_dtype,
+            variant="fp16" if self.torch_dtype == torch.float16 else None,
+            use_safetensors=True
+        )
+        self.pipe.to(self.device)
+        self.a_prompt = 'best quality, extremely detailed'
+        self.n_prompt = 'longbody, lowres, bad anatomy, bad hands, missing fingers, extra digit, ' \
+                        'fewer digits, cropped, worst quality, low quality'
+
+    @prompts(name="Generate Image From User Input Text",
+             description="useful when you want to generate an image from a user input text and save it to a file. "
+                         "like: generate an image of an object or something, or generate an image that includes some objects. "
+                         "if the input text is Chinese, translate into English then do the generation job."
+                         "The input to this tool should be a string, representing the text used to generate image. ")
+    def inference(self, text):
+        image_filename = os.path.join('image', str(uuid.uuid4())[0:8] + ".png")
+        prompt = text + ', ' + self.a_prompt
+        image = self.pipe(
+            prompt=prompt,
+            negative_prompt=self.n_prompt,
+            guidance_scale=7.5
+        ).images[0]
+        image.save(image_filename)
+        print(f"\nProcessed Text2Image, Input Text: {text}, Output Image: {image_filename}")
+        return image_filename    
 
 
 class ImageCaptioning:
